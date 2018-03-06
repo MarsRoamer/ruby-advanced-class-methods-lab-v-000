0318cd37965640add3969d5c839ef3bd2819bc20
Jonathan Tiede <skysoldiermars@gmail.com>
2018-03-06 23:24:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,57 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  # def initialize(name)
+  #   @name = name
+  # end
+
   def self.all
     @@all
   end
 
+
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    @@all.find do |obj|
+      if obj.name == song_name
+        obj
+      end
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if !self.find_by_name(song_name)
+      self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+     @@all.sort_by(&:name)
+   end
+
 
 end
